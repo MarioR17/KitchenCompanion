cf2e16c1de6325c176af1d9ff578cd182167d63b
jblgross <jessiegrosswi@icloud.com>
2025-06-27 14:45:49 -0400

    Changes to the schema

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -8,44 +8,17 @@ CREATE TABLE IF NOT EXISTS Users (
   FOREIGN KEY (cookingLevel) REFERENCES CookingLevel(cookingId)
 );
 
-CREATE TABLE IF NOT EXISTS UsersDietaryRestrictions (
+CREATE TABLE IF NOT EXISTS DietaryRestrictions (
+  restrictionId INTEGER PRIMARY KEY AUTOINCREMENT, 
+  restriction TEXT NOT NULL
   user_id INTEGER,
-  restriction_id INTEGER,
-  PRIMARY KEY (user_id, restriction_id),
-  FOREIGN KEY (user_id) REFERENCES Users(userId),
-  FOREIGN KEY (restriction_id) REFERENCES DietaryRestrictions(restrictionId)
-);
+  FOREIGN KEY (user_id) REFERENCES Users(userId)
+)
 
 CREATE TABLE IF NOT EXISTS DietaryRestrictions (
   restrictionId INTEGER PRIMARY KEY AUTOINCREMENT, 
   restriction TEXT NOT NULL
 );
-
-INSERT OR IGNORE INTO DietaryRestrictions (restriction) VALUES
-('Vegetarian'),
-('Vegan'),
-('Pescatarian'),
-('Kosher'),
-('Halal'),
-('Paleo'),
-('Keto'),
-('Gluten-Free'),
-('Wheat-Free'),
-('Nut-Free'),
-('Peanut-Free'),
-('Tree Nut-Free'),
-('Dairy-Free'),
-('Lactose-Free'),
-('Egg-Free'),
-('Soy-Free'),
-('Shellfish-Free'),
-('Fish-Free'),
-('Sesame-Free'),
-('Low FODMAP'),
-('Low Carb'),
-('Low Sodium'),
-('No Pork'),
-('No Beef');
 
 CREATE TABLE IF NOT EXISTS CookingLevel (
     cookingId INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -54,6 +27,6 @@ CREATE TABLE IF NOT EXISTS CookingLevel (
 );
 
 INSERT OR IGNORE INTO CookingLevel (cookingLevel, cookingDescription) VALUES
-('Beginner', 'New to cooking or rarely cooks. Needs: Detailed step-by-step instructions with photos, explanations of terms (e.g. sauté, dice), simpler recipes with fewer ingredients.'),
-('Confident Home Cook', 'Comfortable following recipes, basic techniques mastered, experiments occasionally. Needs: Standard recipe layout without extra definitions, familiar with basic skills, may need videos or tips for advanced prep like filleting fish.'),
+('Guided Chef', 'New to cooking or rarely cooks. Needs: Detailed step-by-step instructions with photos, explanations of terms (e.g. sauté, dice), simpler recipes with fewer ingredients.'),
+('Confident Chef', 'Comfortable following recipes, basic techniques mastered, experiments occasionally. Needs: Standard recipe layout without extra definitions, familiar with basic skills, may need videos or tips for advanced prep like filleting fish.'),
 ('Advanced Chef', 'Highly experienced home or professional cook. Needs: minimalistic instructions, advanced techniques (e.g. sous vide, confit), flavor pairing suggestions, plating guides, flexibility to modify or build recipes from ingredient suggestions.');
